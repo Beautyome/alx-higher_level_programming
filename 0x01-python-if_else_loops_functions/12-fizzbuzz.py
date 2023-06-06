3d28c5d9bc0881453ef79dfc1728d9fda371351b
Beautyome <Beautyupeleomo@gmail.com>
2023-06-06 08:41:36 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,5 @@ def fizzbuzz():
         n = a % 3 == 0
         h = j and n
         mes = f"FizzBuzz" if h else f"Buzz" if j \
-                else f"Fizz" if n else f"{a:d}"
-                print(mes, end=" ")
+            else f"Fizz" if n else f"{a:d}"
+        print(mes, end=" ")
