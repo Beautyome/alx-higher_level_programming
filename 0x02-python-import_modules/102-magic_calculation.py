28ab091910fd3c22510724d35aaaabf10a083fd7
Beautyome <Beautyupeleomo@gmail.com>
2023-06-08 11:48:33 -0700

    ByteCode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,12 +2,10 @@
 def magic_calculation(c, d):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
-
     if c < d:
         e = add(c, d)
         for n in range(4, 6):
             e = add(e, n)
             return (e)
-
         else:
             return(sub(c, d))
