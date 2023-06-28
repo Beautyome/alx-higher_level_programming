0b35ca57278bfdbfe06b6d80ea3760f29e584de5
Beautyome <beautyupeleomo@gmail.com>
2023-06-28 04:39:57 -0700

    py Classes and Objects

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task"""
+
+
+class Square:
+    """This is a class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+        
