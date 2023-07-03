fdfd00bdc3fca16b1e5e76ec6484e8be20b825ee
Beautyome <beautyupeleomo@gmail.com>
2023-07-03 16:20:08 -0700

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -99,4 +99,4 @@ def recursive_solve(board, row, queens, solutions):
         board (list): The current working chessboard.
         row (int): The current working row.
         queens (int): The current number of placed queens.
-        solutions
+        solutions (list):
