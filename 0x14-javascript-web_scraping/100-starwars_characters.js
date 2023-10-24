a3094abac74f64c77c37c9acb41e22d69f959e8b
Beautyome <beautyupeleomo@gmail.com>
2023-10-24 05:03:47 -0700

    web scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+const id = process.argv[2];
+const endpoint = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const opt = { method: 'GET' };
+
+request(endpoint, opt, (err, { statusCode, body }) => {
+  if (err) return console.log(err);
+  if (statusCode === 200) {
+    const { characters } = JSON.parse(body);
+    characters.forEach(url => {
+      request(url, opt, (err, { statusCode: code, body: res }) => {
+        if (err) return console.log(err);
+        if (code === 200) {
+          return console.log(JSON.parse(res).name);
         }
+        console.log(`Error code: ${code}`);
       });
     });
+  } else {
+    console.log(`Error code: ${statusCode}`);
   }
 });
